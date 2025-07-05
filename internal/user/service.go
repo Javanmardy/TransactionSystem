17c0570d34d61b68a8fdee5f45ef16ab104016e3
Amirhosein Javanmardy <a.javanmardy2004@gmail.com>
2025-07-05 22:17:29 +0330

    Add MySQL user service structure for future DB integration

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,24 @@
-// Sample content for service.go
+package user
+
+type Service interface {
+	GetUserByID(id int) *User
+}
+
+type mockService struct{}
+type mysqlService struct{}
+
+func NewMockService() Service {
+	return &mockService{}
+}
+
+func NewMySQLService() Service {
+	return &mysqlService{}
+}
+
+func (s *mockService) GetUserByID(id int) *User {
+	return GetUserByID(id)
+}
+
+func (s *mysqlService) GetUserByID(id int) *User {
+	return nil
+}
