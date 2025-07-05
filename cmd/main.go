ef7ffd2ad48753c742a3b0adf1ea3ec146e82a1f
Amirhosein Javanmardy <a.javanmardy2004@gmail.com>
2025-07-05 22:46:46 +0330

    Add HTTP handler for listing user transactions and minimal main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
-// Sample content for main.go
+package main
+
+import (
+	"TransactionSystem/internal/transaction"
+	"log"
+	"net/http"
+)
+
+func main() {
+	repo := transaction.NewMockRepo()
+	svc := transaction.NewService(repo)
+	handler := transaction.NewHandler(svc)
+
+	http.HandleFunc("/transactions", handler.ListUserTransactions)
+
+	log.Println("Server running at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- internal/transaction/handler.go
+++ internal/transaction/handler.go
@@ -1,1 +1,32 @@
-// Sample content for handler.go
+package transaction
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	service Service
+}
+
+func NewHandler(s Service) *Handler {
+	return &Handler{service: s}
+}
+
+func (h *Handler) ListUserTransactions(w http.ResponseWriter, r *http.Request) {
+	userIDStr := r.URL.Query().Get("user_id")
+	if userIDStr == "" {
+		http.Error(w, "user_id is required", http.StatusBadRequest)
+		return
+	}
+	userID, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		http.Error(w, "invalid user_id", http.StatusBadRequest)
+		return
+	}
+
+	transactions := h.service.ListUserTransactions(userID)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(transactions)
+}
