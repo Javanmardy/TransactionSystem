965b27a6d43c5e13fa5ce936408f1ee01c06c8ca
Amirhosein Javanmardy <a.javanmardy2004@gmail.com>
2025-07-05 22:21:24 +0330

    Add ReportService to generate user transaction summary

--- internal/report/service.go
+++ internal/report/service.go
@@ -1,1 +1,49 @@
-// Sample content for service.go
+package report
+
+import "TransactionSystem/internal/transaction"
+
+type Report struct {
+	UserID            int
+	TotalTransactions int
+	SuccessfulCount   int
+	FailedCount       int
+	TotalAmount       float64
+}
+
+type Service interface {
+	GenerateUserReport(userID int) *Report
+}
+
+type reportService struct {
+	transactionSvc transaction.Service
+}
+
+func NewService(transactionSvc transaction.Service) Service {
+	return &reportService{
+		transactionSvc: transactionSvc,
+	}
+}
+
+func (s *reportService) GenerateUserReport(userID int) *Report {
+	transactions := s.transactionSvc.ListUserTransactions(userID)
+
+	var success, fail int
+	var amount float64
+
+	for _, t := range transactions {
+		if t.Status == "success" {
+			success++
+		} else {
+			fail++
+		}
+		amount += t.Amount
+	}
+
+	return &Report{
+		UserID:            userID,
+		TotalTransactions: len(transactions),
+		SuccessfulCount:   success,
+		FailedCount:       fail,
+		TotalAmount:       amount,
+	}
+}
