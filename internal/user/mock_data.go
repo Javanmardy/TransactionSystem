a659078730a25d9cc958034f7a20d747295d7c3d
Amirhosein Javanmardy <a.javanmardy2004@gmail.com>
2025-07-05 22:13:12 +0330

    Add mock user list and GetUserByID function

---
+++ internal/user/mock_data.go
@@ -0,0 +1,22 @@
+package user
+
+type User struct {
+	ID       int
+	Username string
+	Email    string
+}
+
+var mockUsers = []User{
+	{ID: 1, Username: "amirhossein", Email: "amirhossein@example.com"},
+	{ID: 2, Username: "armin", Email: "armin@example.com"},
+	{ID: 3, Username: "sara", Email: "sara@example.com"},
+}
+
+func GetUserByID(id int) *User {
+	for _, u := range mockUsers {
+		if u.ID == id {
+			return &u
+		}
+	}
+	return nil
+}
